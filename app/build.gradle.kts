530be91ebfa069527ab21d205faf4380fcea4f48
Hadar <hadar.lachmy@gmail.com>
2026-05-21 01:40:00 +0300

    fix sync project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.pawfectmatch"
+        applicationId = "com.example.matchtail"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
